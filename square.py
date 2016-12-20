0a2f1581f7d419cbf9132cc74dac54112cbe8c4b
Wilcox <MWWilcox@magellanhealth.com>
2016-12-20 12:57:06 -0600

    turtle in the house

--- square.py
+++ square.py
@@ -1,12 +1,21 @@
 import turtle
 
+fred = turtle.Turtle()
+fred.shape("turtle")
+fred.color("black")
+fred.speed(1)
+
+def do_draw ():
+    for i in range(0,4):
+        fred.forward(150)
+        fred.right(90)
+
 def draw_square ():
     window = turtle.Screen()
     window.bgcolor("red")
-    
-    brad = turtle.Turtle()
-    brad.forward(100)
-
+    do_draw()
+    fred.goto(50,30)
+    do_draw()
     window.exitonclick()
 
 draw_square()
