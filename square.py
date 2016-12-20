e6cd9aa14c148c55afd566121f6d9cc924f433dc
Wilcox <MWWilcox@magellanhealth.com>
2016-12-20 14:25:17 -0600

    turtle in the house

--- square.py
+++ square.py
@@ -11,25 +11,32 @@ def move_and_turn(turt, distance, angle):
     turt.forward(distance)
     turt.right(angle)
 
-def draw_triangle(turt):
-    turt.left(270)
-    for i in range(3):
-        move_and_turn(turt, 100, 120)
+#def draw_triangle(turt):
+#    turt.left(270)
+#    for i in range(3):
+#        move_and_turn(turt, 100, 120)
 
 def draw_square(turt):
     for i in range(4):
         move_and_turn(turt, 100, 90)
 
+def turt_turn(turt):
+    move_and_turn(turt, 0, 3)
+
 def draw():
     fred = make_turtle("turtle", "green", 2)
-    wilma = make_turtle("arrow", "pink", 2)
-    dino = make_turtle("circle", "red", 5)
+#    wilma = make_turtle("arrow", "pink", 2)
+#    dino = make_turtle("circle", "red", 5)
 
     window = turtle.Screen()
     window.bgcolor("black")
-    draw_square(fred)
-    draw_triangle(wilma)
-    dino.circle(100)
+    for i in range(36):
+        draw_square(fred)
+        turt_turn(fred)
+    
+#    draw_square(fred)
+#    draw_triangle(wilma)
+#    dino.circle(100)
     window.exitonclick()
 
 draw()
