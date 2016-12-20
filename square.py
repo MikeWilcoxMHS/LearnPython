24fe2003d9cb57192ded7c5509378e3fda38c04f
Wilcox <MWWilcox@magellanhealth.com>
2016-12-20 14:18:59 -0600

    turtle in the house

--- square.py
+++ square.py
@@ -1,31 +1,35 @@
 import turtle
 
-fred = turtle.Turtle()
-fred.shape("turtle")
-fred.color("black")
-fred.speed(4)
-wilma = turtle.Turtle()
-wilma.shape("arrow")
-wilma.color("blue")
-wilma.speed(4)
+def make_turtle(shape, color, speed):
+    turt = turtle.Turtle()
+    turt.shape(shape)
+    turt.color(color)
+    turt.speed(speed)
+    return turt
 
-def do_draw ():
-    for i in range(0,4):
-        fred.forward(150)
-        fred.right(90)
+def move_and_turn(turt, distance, angle):
+    turt.forward(distance)
+    turt.right(angle)
 
-def draw_square ():
+def draw_triangle(turt):
+    turt.left(270)
+    for i in range(3):
+        move_and_turn(turt, 100, 120)
+
+def draw_square(turt):
+    for i in range(4):
+        move_and_turn(turt, 100, 90)
+
+def draw():
+    fred = make_turtle("turtle", "green", 2)
+    wilma = make_turtle("arrow", "pink", 2)
+    dino = make_turtle("circle", "red", 5)
+
     window = turtle.Screen()
-    window.bgcolor("red")
-    do_draw()
-    fred.penup()
-    fred.goto(50,30)
-    fred.pendown()
-    do_draw()
-    wilma.circle(100)
+    window.bgcolor("black")
+    draw_square(fred)
+    draw_triangle(wilma)
+    dino.circle(100)
     window.exitonclick()
 
-
-draw_square()
-
-
+draw()
