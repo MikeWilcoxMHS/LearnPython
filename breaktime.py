3e8920d3934b188b34d4849238cf1084ea438b55
Wilcox <MWWilcox@magellanhealth.com>
2016-12-20 09:36:59 -0600

    v0.0.3

--- breaktime.py
+++ breaktime.py
@@ -4,6 +4,8 @@ import time
 total_breaks = 3
 break_count = 0
 
+print("This program started on "+time.ctime())
+
 while(break_count < total_breaks):
     time.sleep(10)
     webbrowser.open("https://www.youtube.com/watch?v=guKoNCQFAFk&list=PLjzeyhEA84sQKuXp-rpM1dFuL2aQM_a3S")
