697a34d5413aff510f674a2d528c67004091a368
Wilcox <MWWilcox@magellanhealth.com>
2016-12-20 12:32:47 -0600

    named file turtle.py, this confused python and it couldn't find the turtle module

--- breaktime.py
+++ breaktime.py
@@ -7,7 +7,7 @@ break_count = 0
 print("This program started on "+time.ctime())
 
 while(break_count < total_breaks):
-    time.sleep(10)
+    time.sleep(2*60*60)
     webbrowser.open("https://www.youtube.com/watch?v=guKoNCQFAFk&list=PLjzeyhEA84sQKuXp-rpM1dFuL2aQM_a3S")
     break_count = break_count + 1
 
--- rename_files.py
+++ rename_files.py
@@ -1,6 +1,6 @@
 import os
 
-photo_directory = ("C:\Users\mwwilcox\Training\Udacity-Python\prank\prank")
+photo_directory = ("C:\Users\mwwilcox\Training\Udacity-Python\prank\mymessage")
 def rename_files():
     #(1) get file names from a folder
     #file_list = os.listdir(r"C:\Users\mwwilcox\Training\Udacity-Python\prank\prank")
@@ -11,6 +11,8 @@ def rename_files():
     #(2) for each file, rename filename
     os.chdir(photo_directory)
     for file_name in file_list:
+        print ("Old file name: "+file_name)
+        print ("New file name: "+(file_name.translate(None, "0123456789")))
         os.rename(file_name, file_name.translate(None, "0123456879"))
     os.chdir(save_path)
 rename_files()
