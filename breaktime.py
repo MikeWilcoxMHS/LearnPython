83709d62680f8a45fe0559c61a23affdd2d6c5c8
Wilcox <MWWilcox@magellanhealth.com>
2016-12-20 09:19:41 -0600

    version 0.0.1 s Please enter the commit message for your changes. Lines starting

---
+++ breaktime.py
@@ -0,0 +1,15 @@
+import webbrowser
+import time
+
+total_breaks = 3
+break_count = 0
+
+while(break_count < total_breaks):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=guKoNCQFAFk&list=PLjzeyhEA84sQKuXp-rpM1dFuL2aQM_a3S")
+    break_count = break_count + 1
+
+for i in range(0,3):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=guKoNCQFAFk&list=PLjzeyhEA84sQKuXp-rpM1dFuL2aQM_a3S")
+
